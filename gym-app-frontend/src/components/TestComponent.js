294a7c383a3e12b2fe993a621446640030873257
JOHNSON KHADKA <er.johnsonkhadka@gmail.com>
2022-11-19 15:30:46 +0545

    updated the folder structure

---
+++ gym-app-frontend/src/components/TestComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TestComponent = () => {
+  return (
+    <div>Hello React App</div>
+  )
+}
+
+export default TestComponent
--- src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-
-// Connection to DBSetup using mongoose 
-mongoose.connect('mongodb://127.0.0.1:27017/gym-app',{ 
-    useNewUrlParser: true,
-    useUnifiedTopology : true
-})
-
